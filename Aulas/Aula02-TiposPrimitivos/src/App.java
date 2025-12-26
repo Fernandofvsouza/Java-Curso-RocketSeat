e5b18ded66d5200f2743fd7ce8e11f8808aa946f
Fernando souza <fernandofvsouza@icloud.com>
2025-12-26 14:44:55 +0000

    java

---
+++ Aulas/Aula02-TiposPrimitivos/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Tipos primitivos sempre terão um valor padrão quando nao sao declarados
+        //Ex:
+        byte b; //b=0
+        short s; //s=0
+        int i; //i=0
+        long l; //l=0
+
+        float f; //f=0.0
+        double d; //d=0.0
+
+        char c; //c='\u0000'
+        boolean o; //b=false
+
+    }
+}
