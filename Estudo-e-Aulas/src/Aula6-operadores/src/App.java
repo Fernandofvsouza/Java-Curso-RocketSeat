1268bfafcf8e042468f8c2af6d6cc2f04d2ea132
Fernando souza <fernandofvsouza@icloud.com>
2025-12-27 15:37:48 +0000

    Estudo de java

---
+++ Estudo-e-Aulas/src/Aula6-operadores/src/App.java
@@ -0,0 +1,50 @@
+
+
+public class App {
+   public static void main(String[] args) {
+    //Operadores
+    /*
+    operadores aritméticos:
+    = Atribuição
+    + Adição ou concatenação de strings
+    - Subtração
+    * Multiplicação
+    / Divisão
+    % Resto da divisão
+    */
+
+    /*
+    operadores relacionais:
+    == Igualdade
+    != Diferença
+    > Maior que
+    < Menor que 
+    >= Maior ou igual a
+    */
+
+    /*
+    operadores lógicos:
+    && E
+    || Ou
+    ! Não
+     */
+    /**
+     Operadores unarios:
+        ++ Incremento
+        -- Decremento
+     */
+    //Ex
+    int numero = +5;
+    System.out.println("Número: " + -numero);
+    /*
+    operador ternario:
+    condição ? valor_se_verdadeiro : valor_se_falso;
+    */
+   //Ex:
+    int a, b;
+    a = 5;
+    b = 6;
+    String resultado = (a == b) ? "Verdadeiro" : "Falso";
+    System.out.println(resultado);
+   }
+}
