d9346a7a64e39943fdcb0993161743abad879994
Fernando souza <fernandofvsouza@icloud.com>
2026-01-09 17:34:25 +0000

    estudo

---
+++ 3-controleDeFluxo/Aula14-do-while/src/ExemploDoWhile.java
@@ -0,0 +1,15 @@
+/*
+A diferença entre o while e o do while é que o while verifica a condição antes de executar o bloco de código, enquanto o do while executa o bloco de código pelo menos uma vez antes de verificar a condição.
+    sintaxe do do while:
+    do{
+        //bloco de código a ser repetido
+    }while(condição);
+    //Exemplo: joaozinho decidiu ligar para seu amigo dizendo que hoje se entupiu de comer doces
+*/
+
+import java.util.Random;
+public class ExemploDoWhile{
+    public static void main(String[] args) throws Exception {
+      
+    }
+}
