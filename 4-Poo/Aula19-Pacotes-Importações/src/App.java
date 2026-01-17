4150e2f29bf4a60bf36705c6f2df3167a3999587
Fernando souza <fernandofvsouza@icloud.com>
2026-01-17 16:27:22 +0000

    up

---
+++ 4-Poo/Aula19-Pacotes-Importações/src/App.java
@@ -0,0 +1,22 @@
+//Pacotes e importações
+/*
+Pacotes são usados para organizar classes e evitar conflitos de nomes em projetos Java maiores. Eles funcionam como diretórios que agrupam classes relacionadas. Por exemplo, você pode ter um pacote chamado "com.exemplo.utilidades" que contém várias classes utilitárias.
+dentro dos pacotes podemos ter subpacotes, geralmente damos o nome do subpacote com o nome do pacote pai + um ponto + o nome do subpacote, por exemplo:
+*Model: armazena as classes que representam os dados e a lógica de negócio da aplicação.
+*View: contém as classes responsáveis pela interface do usuário e a apresentação dos dados.
+*Controller: gerencia a comunicação entre o Model e a View, processando as entradas do usuário e atualizando a interface conforme necessário.
+*services: contém classes que fornecem funcionalidades específicas, como acesso a banco de dados, autenticação, etc.
+*utils: armazena classes utilitárias e funções auxiliares que podem ser usadas em várias partes da aplicação.
+
+
+ */
+
+
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
