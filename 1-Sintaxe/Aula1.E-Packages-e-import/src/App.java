5610b4dfcf2ca808a33a83116002eae785b0129d
Fernando souza <fernandofvsouza@icloud.com>
2026-01-18 19:24:34 +0000

    up

--- 1-Sintaxe/Aula1.E-Packages-e-import/src/App.java
+++ 1-Sintaxe/Aula1.E-Packages-e-import/src/App.java
@@ -1,14 +1,26 @@
 //Vamos supor que estamos criando um software de streaming com as seguintes classes:
 /*
+pacotes representando categorias de vídeos:
 Filme.java
 Serie.java
 Anime.java
 Novela.java
 Documentario.java
-Janela.java
-MiniaturaVideo.java
-BotaoPlay.java
-BotaoPause.java
-BotaoVoltar.java
+-----------------
+pacotes representando a interface do usuário ou gui (graphical user interface):
+    Janela.java
+    MiniaturaVideo.java
+        ----------------
+        subcategorias botoes de controle de vídeo:
+        BotaoPlay.java
+        BotaoPause.java
+        BotaoVoltar.java
+------------------
 App.java
 */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Software de Streaming Iniciado");
+    }
+}
