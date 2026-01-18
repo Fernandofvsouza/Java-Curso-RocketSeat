09e5c41fa16f454e98888563ca0ad1b0437c7f10
Fernando souza <fernandofvsouza@icloud.com>
2026-01-18 20:01:24 +0000

    up

---
+++ 1-Sintaxe/Aula1.e-packagesEimports/src/app/App.java
@@ -0,0 +1,27 @@
+//Vamos supor que estamos criando um software de streaming com as seguintes classes:
+/*
+pacotes representando categorias de vídeos:
+Filme.java
+Serie.java
+Anime.java
+Novela.java
+Documentario.java
+-----------------
+pacotes representando a interface do usuário ou gui (graphical user interface):
+    Janela.java
+    MiniaturaVideo.java
+        ----------------
+        subcategorias botoes de controle de vídeo:
+        BotaoPlay.java
+        BotaoPause.java
+        BotaoVoltar.java
+------------------
+App.java
+*/
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
