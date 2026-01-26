f840aa4a497d65fb10aa2866ebb35c2b822d4fae
Fernando souza <fernandofvsouza@icloud.com>
2026-01-26 19:33:28 +0000

    encapsulamento

---
+++ 5-pilares-poo/Aula22-introdução/src/ComputadorPedrinho.java
@@ -0,0 +1,9 @@
+public class ComputadorPedrinho {
+    public static void main(String[] args) {
+        MSNmessenger msn = new MSNmessenger(); //Pedrinho instalou o MSN Messenger em seu computador
+        //msn.validarConexaoInternet();
+        msn.enviarMensagem();
+        msn.receberMensagem();
+        //msn.salvarHistoricoConversas();
+    }
+}
